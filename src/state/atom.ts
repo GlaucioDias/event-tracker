b3bdb24e0644dcbc1388d2526b29c4d26ad75b32
Glaucio <glaucio@pop-os.localdomain>
2023-03-08 13:30:04 -0300

    Added project

---
+++ src/state/atom.ts
@@ -0,0 +1,14 @@
+import { eventosAsync } from './seletores/index';
+import { IFiltroDeEventos } from './../interfaces/IFiltroDeEventos';
+import { IEvento } from './../interfaces/IEvento';
+import { atom } from 'recoil'
+
+export const listaDeEventosState = atom<IEvento[]>({
+    key: 'listaDeEventosState',
+    default: eventosAsync
+})
+
+export const filtroDeEventos = atom<IFiltroDeEventos>({
+  key: 'filtroDeEventos',
+  default: {}
+})
